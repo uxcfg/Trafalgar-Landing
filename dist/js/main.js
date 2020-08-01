9f5de3ae8c594073a2944d7f8322feb49143b958
Uxcfg <lalalal@lalala.la>
2020-08-01 21:59:55 +0300

    complete

--- dist/js/main.js
+++ dist/js/main.js
@@ -21,3 +21,27 @@ const mySwiper = new Swiper('.swiper-container', {
 		el: '.swiper-pagination',
 	},
 });
+
+const reviews = new Swiper('.swiper-reviews', {
+	// Optional parameters
+	direction: 'horizontal',
+	loop: true,
+	spaceBetween: 50,
+	effect: 'slide',
+
+	// If we need pagination
+	pagination: {
+		el: '.swiper-pagination',
+	},
+
+	// Navigation arrows
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+
+	// And if we need scrollbar
+	scrollbar: {
+		el: '.swiper-scrollbar',
+	},
+});
